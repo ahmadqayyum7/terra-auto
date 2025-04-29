2503a976f0b0eaacdf1238ed6906adc1443b8a99
ahmadqayyum7 <ahmadqayyum19@gmil.com>
2025-04-29 22:26:48 +0500

    Adding S3 and state locking

--- s3.tf
+++ s3.tf
@@ -1,9 +1,9 @@
 resource "s3_bucket" "my_bucket" {
 
-    bucket = "my-s3-bucket"
+    bucket = "my-s3-bucket-auto"
 
 tags = {
-    Name    = "my-s3-bucket"
+    Name    = "my-s3-bucket-auto"
     
   }
 }
